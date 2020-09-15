df2a0c5288b7b64a52e6300dafae6a2aa989580b
Khanh-Nobi-JR <69251285+Khanh-Nobi-JR@users.noreply.github.com>
2020-09-15 16:32:06 +0700

    add

---
+++ src/TestCircle.java
@@ -0,0 +1,9 @@
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        circle.setColor("yellow");
+        circle.setRadius(2.0);
+        System.out.println(circle.getColor());
+        System.out.println(circle.getRadius());
+    }
+}
